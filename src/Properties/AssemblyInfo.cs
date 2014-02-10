158796258b76f5aba5a43001de2c73854f14da19
Jaben <jaben.cargman@captiveaire.com>
2014-02-10 08:27:49 -0500

    Forgot to change the assembly name.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Seq.Apps.YouTrack")]
+[assembly: AssemblyTitle("Seq.App.YouTrack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Seq.Apps.YouTrack")]
+[assembly: AssemblyProduct("Seq.App.YouTrack")]
 [assembly: AssemblyCopyright("Copyright CaptiveAire © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
